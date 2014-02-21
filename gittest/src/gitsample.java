c916a192a0d28b0fb070a88233768065eba9c734
ineshKCWA <ineshgunasekara@ymail.com>
2014-02-22 01:25:48 +0530

    Update gitsample.java
    use inside website

--- gittest/src/gitsample.java
+++ gittest/src/gitsample.java
@@ -1,10 +1,10 @@
-
 public class gitsample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
 		System.out.println("this is the 2nd comment");
+		System.out.println("try via browser to eclipse");
 	}
 
 }
